7fb6060a6409aae1a763411c393867002590ba56
kamiloleszek <oleszek101@wp.pl>
2017-11-14 19:42:02 +0100

    Created model Courses and made migration. Created url configurations for courses view

--- app/models.py
+++ app/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+	course_id = models.BigIntegerField(primary_key=True)
+	name = models.CharField(max_length=30)
+
+	class Meta:
+		ordering = ['name']
+	
+	def __str__(self):
+		return (self.name)
