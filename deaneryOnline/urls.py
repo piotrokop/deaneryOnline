9e7c2a0b63be3bf9f8d8d4da97ac4617d459a626
Michał <mpoznanski@interia.pl>
2017-11-16 23:57:36 +0100

    login page works in newest django

--- deaneryOnline/urls.py
+++ deaneryOnline/urls.py
@@ -20,8 +20,8 @@ from django.contrib.auth import views as auth_views
 from app.views import createview, courses
 
 urlpatterns = [
-    url(r'^login/', 'django.contrib.auth.views.login'),
-    url(r'^logout/', 'django.contrib.auth.views.logout'),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout),
     url(r'^accounts/profile/', createview),
     url(r'^admin/', admin.site.urls),
     #url(r'^courses/', include('app.urls')),
