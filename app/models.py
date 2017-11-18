63d41c5a177eaa8e1dbff2772e88ea74c6d9f971
Weronika <weronika.weglinska@gmail.com>
2017-11-18 02:06:52 +0100

    Added models representing users and their roles + sample data

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -14,3 +17,33 @@ class Course(models.Model):
 	
 	def __str__(self):
 		return (self.name)
+
+
+class UserRole(models.Model):
+	name = models.CharField(max_length=15)
+
+	def __str__(self):
+		return self.name
+
+#Profile and default User model are related as one to one
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	role_id = models.ForeignKey(UserRole, on_delete=models.CASCADE, null=True)
+	semester = models.IntegerField(null=True)
+
+	@receiver(post_save, sender=User)
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User)
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
+
+
+#Method injection to User class
+def get_full_name(self):
+	return '%s %s' % (self.last_name, self.first_name)
+
+User.add_to_class("__str__", get_full_name)
+
