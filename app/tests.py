49fe7b21e8cdd29d1398bcbdf07cc92fd1292876
4papiez <4papiez@fis.agh.edu.pl>
2017-12-09 21:36:58 +0100

    Added test file for tests

--- app/tests.py
+++ app/tests.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestTestCase(TestCase):
+	def test_jeden(self):
+		pass
+	
+	def test_dwa(self):
+		pass
