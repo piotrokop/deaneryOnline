eb820fd464dc6078a20e41867a783cacbc477729
Michał <mpoznanski@interia.pl>
2017-12-12 22:47:18 +0100

    grades are downloading from database in manage course, manage_course view refactor

--- grade/forms.py
+++ grade/forms.py
@@ -7,22 +7,41 @@ from grade.models import UserGrade
 		
 		
 class ManageCourseForm(forms.ModelForm):
-	GRADE_CHOICES = (
+    GRADE_CHOICES = (
         ('None', 'None'),
-		(2.0, '2.0'),
-		(3.0, '3.0'),
-		(3.5, '3.5'),
-		(4.0, '4.0'),
-		(4.5, '4.5'),
-		(5.0, '5.0'),
-	)
-	exercises = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
-	laboratories = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
-	project = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
-	seminars = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
-	exam = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
-	final_grade = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+        (2.0, '2.0'),
+        (3.0, '3.0'),
+        (3.5, '3.5'),
+        (4.0, '4.0'),
+        (4.5, '4.5'),
+        (5.0, '5.0'),
+    )
+    exercises = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+    laboratories = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+    project = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+    seminars = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+    exam = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
+    final_grade = forms.ChoiceField(choices=GRADE_CHOICES, required = False)
 	
-	class Meta:
-		model = UserGrade
-		fields = ('exercises', 'laboratories', 'project', 'seminars', 'exam', 'final_grade', )
+    class Meta:
+        model = UserGrade
+        fields = ('exercises', 'laboratories', 'project', 'seminars', 'exam', 'final_grade')
+        
+    def save(self, course, user_courses, user_index, user_id, course_id):
+        data = self.cleaned_data
+        for field in self:
+            if data[field.name] != 'None' and data[field.name] != '' and (field.name == 'final_grade' or getattr(course, field.name) > 0):
+                usergrade, created = UserGrade.objects.get_or_create(
+                    category=field.name,
+                    student_user_id=user_courses[user_index].profile.user_id,
+                    course_id=course_id,
+                    defaults={'grade': 3.0, 'is_final': False, 'professor_user_id': user_id}
+                )
+                if created:
+                    usergrade.is_final = True
+                    usergrade.course_id = course_id
+                    usergrade.category = field.name
+                    usergrade.professor_user_id = user_id
+                    usergrade.student_user_id = user_courses[user_index].profile.user_id
+                usergrade.grade = data[field.name]
+                usergrade.save()
--- grade/views.py
+++ grade/views.py
@@ -16,37 +16,38 @@ from course.models import Course
 from user.models import UserCourse
 
 def grades(request):
-	if request.user.is_authenticated():
-		user = DBHelper.get_user(request)
-		grades = UserGrade.objects.filter(student_user_id=user, is_final=True)
-		course_set = Set()
-		courses = []
-		grade_set = (grade for grade in grades )
-		for grade in grade_set:
-			course_set.add(grade.course)
-		for c in course_set:
-			course = Course.objects.get(course_id=c.course_id)
-			courses.append({'Name' : course.name ,'Id' : course.course_id , 'ECTS' : course.ects, 'Pos' : len(courses)})
-		for grade in grades:
-			for c in courses:
-				if c['Id']==grade.course_id:
-					courses[c['Pos']][grade.category]=grade.grade
-		sum,pkt =0.0,0
-		for c in courses:
-			sum += float(c['final_grade']) * c['ECTS']
-			pkt += c['ECTS'] if c['final_grade'] else 0
-		return render(request, 'grades.html', {
+    if request.user.is_authenticated():
+        user = DBHelper.get_user(request)
+        grades = UserGrade.objects.filter(student_user_id=user, is_final=True)
+        course_set = Set()
+        courses = []
+        grade_set = (grade for grade in grades )
+        for grade in grade_set:
+            course_set.add(grade.course)
+        for c in course_set:
+            course = Course.objects.get(course_id=c.course_id)
+            courses.append({'Name' : course.name ,'Id' : course.course_id , 'ECTS' : course.ects, 'Pos' : len(courses)})
+        for grade in grades:
+            for c in courses:
+                if c['Id']==grade.course_id:
+                    courses[c['Pos']][grade.category]=grade.grade
+        sum,pkt =0.0,0
+        for c in courses:
+            if 'final_grade' in c:
+                sum += float(c['final_grade']) * c['ECTS']
+                pkt += c['ECTS'] if c['final_grade'] else 0
+        return render(request, 'grades.html', {
             "role_obj": user.role,
             "gradeList" : courses,
-			"average" : "{0:.2f}".format((sum/pkt) if pkt else 0)
+            "average" : "{0:.2f}".format((sum/pkt) if pkt else 0)
         })
 		
 		
 	
 def course_manage(request, id):
     course = Course.objects.get(course_id = id)
-    user_courses = UserCourse.objects.filter(course=course, accepted=1)
-    ManageCourseFormSet = formset_factory(ManageCourseForm, extra = user_courses.count())
+    user_courses = UserCourse.objects.filter(course=course, accepted=1, profile__role=1)
+    ManageCourseFormSet = formset_factory(ManageCourseForm, extra = 0)
     zipped = []
     users_list = [(user_course.profile.user.first_name, user_course.profile.user.last_name) for user_course in user_courses]
     if request.method == 'POST':
@@ -54,28 +55,20 @@ def course_manage(request, id):
         if formset.is_valid():
             user_index = 0
             for form in formset:
-                data = form.cleaned_data
+                form.save(course, user_courses, user_index, request.user.id, id)
                 zipped.append((form, users_list[user_index]))
-                for field in form:
-                    if data[field.name] != 'None' and data[field.name] != '' and (field.name == 'final_grade' or getattr(course, field.name) > 0):
-                        usergrade, created = UserGrade.objects.get_or_create(
-                            category=field.name,
-                            student_user_id=user_courses[user_index].profile.user_id,
-                            course_id=id,
-                            defaults={'grade': 3.0, 'is_final': False, 'professor_user_id': request.user.id}
-                        )
-                        if created:
-                            usergrade.is_final = True
-                            usergrade.course_id = id
-                            usergrade.category = field.name
-                            usergrade.professor_user_id = request.user.id
-                            usergrade.student_user_id = user_courses[user_index].profile.user_id
-                        usergrade.grade = data[field.name]
-                        usergrade.save()
                 user_index += 1
     else:
         user_index = 0
-        formset = ManageCourseFormSet()
+        initial_grades = []
+        for user_course in user_courses:
+            profile = user_course.profile
+            grades = UserGrade.objects.filter(student_user = profile, course=course)
+            grades_dict = {}
+            for grade in grades:
+                grades_dict[grade.category] = grade.grade
+            initial_grades.append(grades_dict)
+        formset = ManageCourseFormSet(initial=initial_grades)
         for form in formset:
             zipped.append((form, users_list[user_index]))
             user_index += 1
